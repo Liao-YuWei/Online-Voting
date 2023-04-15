b8e3b16598c8881c27b5c2b9a8feb534c05fcf5d
minyunh <mina880709@gmail.com>
2023-04-15 15:39:08 +0800

    lab2

--- src/client.py
+++ src/client.py
@@ -1,47 +1,97 @@
 from __future__ import print_function
 
 import logging
+from time import sleep
 
 import grpc
+import random
 
 import eVoting_pb2
 import eVoting_pb2_grpc
 from google.protobuf.timestamp_pb2 import Timestamp
+
+# HW 2
+from nacl.signing import SigningKey
+from datetime import datetime, timedelta
+import base64
 
 def run():
     port = '34652'
     channel = grpc.insecure_channel('localhost:' + port)
     stub = eVoting_pb2_grpc.eVotingStub(channel)
 
-    response = stub.PreAuth(eVoting_pb2.VoterName(name = 'Alice'))
-    print('Response message of PreAuth.')
-    print(f'Challenge: {response.value.decode()}\n')
+    # HW2: Reading private key 
+    with open("private_key", "rb") as f:
+        key = f.read()
 
-    response = stub.Auth(eVoting_pb2.AuthRequest(name = eVoting_pb2.VoterName(name = 'Alice'), \
-                                                 response = eVoting_pb2.Response(value = '1'.encode())))
-    print('Response message of Auth')
-    print(f'AuthToken: {response.value.decode()}\n')
+    sign_key = SigningKey(key)
+    
+    # HW2: PreAuth
+    pre_response = stub.PreAuth(eVoting_pb2.VoterName(name = "Vidar"))
+
+    # HW2: Auth
+    challenge = pre_response.value
+    signed = sign_key.sign(challenge)
+    signature = signed.signature
+
+    auth_response = stub.Auth(eVoting_pb2.AuthRequest(
+        name = eVoting_pb2.VoterName(name = "Vidar"),\
+        response = eVoting_pb2.Response(value = bytes(signature))\
+    ))
+
+    token = auth_response.value
+
+    # response = stub.PreAuth(eVoting_pb2.VoterName(name = 'Alice'))
+    print('Response message of PreAuth.')
+    print(f'PreAuth: {pre_response.value}\n')
+
+    print('Response message of Auth.')
+    print(f'Auth: {auth_response.value}\n')
+
+    # response = stub.Auth(eVoting_pb2.AuthRequest(name = eVoting_pb2.VoterName(name = 'Alice'), \
+    #                                              response = eVoting_pb2.Response(value = '1'.encode())))
+    # print('Response message of Auth')
+    # print(f'AuthToken: {response.value.decode()}\n')
+
+    # election config 
+    election_name = 'what color do you like ?'
+    groups = ["Group A", "Group B"]
+    choices = ["Red", "Blue", "White", "Yellow"]
+    due = datetime.now() + timedelta(seconds=2)
 
     curElection = eVoting_pb2.Election()
-    curElection.name = 'Election A'
-    curElection.groups.append('Group A')    #extend
-    curElection.choices.append('Bob')
-    curElection.end_date.GetCurrentTime()
-    curElection.token.value = '2'.encode()
-    response = stub.CreateElection(curElection)
+    curElection.name = election_name
+    curElection.groups.extend(groups)
+    curElection.choices.extend(choices)
+    curElection.end_date.FromDatetime(due)
+    curElection.token.value = bytes(token)
+
+    creat_election_response = stub.CreateElection(curElection)
     print('Response message of CreateElection')
-    print(f'Status: {response.code}\n')
+    print(f'Status: {creat_election_response.code}\n')
 
-    response = stub.CastVote(eVoting_pb2.Vote(election_name = 'Election A', \
-                                                   choice_name = 'Bob', \
-                                                    token = eVoting_pb2.AuthToken(value = '3'.encode())))
+    # call castvote
+    r = random.randint(0, 3)
+    castvote_response = stub.CastVote(eVoting_pb2.Vote(election_name = election_name, \
+                                                   choice_name = choices[r], \
+                                                    token = eVoting_pb2.AuthToken(value = bytes(token))))
+    
     print('Response message of CastVote')
-    print(f'Status: {response.code}\n')
+    print(f'Status: {castvote_response.code}\n')
+    
+    sleep(1)
+    # call getresult
+    get_result_response = stub.GetResult(eVoting_pb2.ElectionName(name = election_name))
+    print('Response message of GetResult ')
+    print(f'Status: {get_result_response.status}\n')
 
-    response = stub.GetResult(eVoting_pb2.ElectionName(name = 'Election A'))
-    print('Response message of CastVote')
-    print(f'Status: {response.status}')
-    print(f'VoteCount: {response.counts[0].choice_name}, {response.counts[0].count}')
+    
+    # result
+    for votecnt in get_result_response.counts:
+        print("Choice {} got {} ballots in election!".format(votecnt.choice_name, votecnt.count))
+
+
+    
 
 
 if __name__ == '__main__':
--- src/server.py
+++ src/server.py
@@ -4,58 +4,234 @@ import signal
 import sys
 
 import grpc
+import nacl
 
 import eVoting_pb2
 import eVoting_pb2_grpc
 
-from datetime import datetime
+# HW 2: Public key generation using libsodium dependencies
+import secrets
+import nacl.utils
+from nacl.signing import SigningKey
+from nacl.signing import VerifyKey
+from nacl.encoding import Base64Encoder
+import pyrqlite.dbapi2 as dbapi2
+from math import log
 
-def RegisterVoter(voter: eVoting_pb2.Voter) -> eVoting_pb2.Status:
-    pass
+from datetime import datetime, timedelta
 
-def UnregisterVoter(votername: eVoting_pb2.VoterName) -> eVoting_pb2.Status:
-    pass
+class Server:
 
+    registration_table = {}
+    challenge_table = {}
+    token_table = {}
+    election_table = {}
+
+
+    def check_voter(self, data):
+        print(data)
+
+    # HW 2, A: Local Server API (RegisterVoter, UnregisterVoter)
+    def RegisterVoter(self, voter: eVoting_pb2.Voter) -> eVoting_pb2.Status:
+        index = voter.name
+        print(f'1. Registering voter {index}')
+        if index not in self.registration_table:
+            public_key = VerifyKey(voter.public_key)
+            self.registration_table[index] = {"group": voter.group, "public_key": public_key}
+            print(f"Voter: {index} is registered")
+            return eVoting_pb2.Status(code=0)
+        else:
+            print(f"Voter {index} is already registered")
+            return eVoting_pb2.Status(code=1)
+        print("-------------Reg done(2)-------------\n")
+        return eVoting_pb2.Status(code=2)
+
+    def UnregisterVoter(self, votername: eVoting_pb2.VoterName) -> eVoting_pb2.Status:
+        index = votername.name
+        print(f'2. Unregistering voter {index}')
+        if index in self.registration_table:
+            self.registration_table.pop(index)
+            print("Voter is unregistered")
+            return eVoting_pb2.Status(code=0)
+        else:
+            print("Voter doesn't exist")
+            return eVoting_pb2.Status(code=1)
+        
+        return eVoting_pb2.Status(code=2)
+
+
+# HW 2, B: RPC APIs
 class eVotingServicer(eVoting_pb2_grpc.eVotingServicer):
+
+    def __init__(self):
+        self.server = Server()
+        sign_key = SigningKey.generate()
+        with open("private_key", "wb") as f:
+            f.write(sign_key.encode())
+        verify_key = sign_key.verify_key
+        verify_key_bytes = verify_key.encode()
+
+        # HW 2: Verify that register works (register)
+        self.server.RegisterVoter(eVoting_pb2.Voter(name="Vidar", group="Group A", public_key=verify_key_bytes))
+        print(self.server.registration_table)
+        print("-------------Reg done(0)-------------\n")
+
+        # HW 2: Verify that register works (duplicate)
+        self.server.RegisterVoter(eVoting_pb2.Voter(name="Vidar", group="Group A", public_key=verify_key_bytes))
+        print(self.server.registration_table)
+        print("-------------Reg done(1)-------------\n")
+
+        # HW 2: Verify that voter works (unregister)
+        self.server.UnregisterVoter(eVoting_pb2.VoterName(name="Vidar"))
+        print(self.server.registration_table)
+        print("-------------UnReg done(0)-------------\n")
+        
+        # HW 2: Verify that voter works (exists)
+        self.server.UnregisterVoter(eVoting_pb2.VoterName(name="Vidar"))
+        print(self.server.registration_table)
+        print("-------------UnReg done(1)-------------\n")
+
+        # HW 2: Verify that register works (re-register for next part)
+        self.server.RegisterVoter(eVoting_pb2.Voter(name="Vidar", group="Group A", public_key=verify_key_bytes))
+        print(self.server.registration_table)
+        print("-------------Re-reg done(0)-------------\n")
+
+
+    # HW2: RPC call
     def PreAuth(self, request, context):
-        print("Received PreAuth.")
-        print(f'Name: {request.name}\n')
-        return eVoting_pb2.Challenge(value = '1'.encode())
+        voter_name = request
+        key = voter_name.name
+        challenge = secrets.token_bytes(4) # challenge_size = 4
+        print(f"1. Received PreAuth. Voter: {key}")
+        self.server.challenge_table[key] = challenge # add challenge
+        print("-------------PreAuth done-------------\n")
+        return eVoting_pb2.Challenge(value = bytes(challenge))
 
     def Auth(self, request, context):
-        print("Received Auth.")
-        print(f'Name: {request.name.name}')
-        print(f'Response: {request.response.value.decode()}\n')
-        return eVoting_pb2.AuthToken(value = '2'.encode())
+        # print(f'Name: {request.name.name}')
+        # print(f'Response: {request.response.value.decode()}\n')
+
+        auth_req = request
+        index = auth_req.name.name
+        print(f"2. Received Auth. Name: {index}")
+        challenge = self.server.challenge_table[index] # get index
+        public_key = self.server.registration_table[index]["public_key"] # get index's public key
+        signature = auth_req.response.value
+
+        try:
+            public_key.verify(smessage=challenge, signature = signature)
+        except:
+            return eVoting_pb2.AuthToken(value = bytes("invalid", encoding="utf-8"))
+
+        token = secrets.token_bytes(4)  # token size = 4
+        expired = datetime.now()+timedelta(hours=1)
+        self.server.token_table[token] = {"expired": expired, "name": index}
+        print("-------------Auth done-------------\n")
+        return eVoting_pb2.AuthToken(value = bytes(token))
 
     def CreateElection(self, request, context):
         print("Received CreateElection.")
         print(f'Name: {request.name}')
-        print(f'Groups: {request.groups[0]}')
-        print(f'Choices: {request.choices[0]}')
+        print(f'Groups: {request.groups}')
+        print(f'Choices: {request.choices}')
         print(f'Time: {datetime.fromtimestamp(request.end_date.seconds)}')
-        print(f'Token: {request.token.value.decode()}\n')
-        return eVoting_pb2.Status(code = 3)
+        print(f'Token: {request.token.value}\n')
+
+        try:
+            # Status.code=1 : Invalid authentication token
+            if request.token.value not in self.server.token_table:
+                return eVoting_pb2.Status(code = 1)
+            if datetime.now() >= self.server.token_table[request.token.value]["expired"]:
+                return eVoting_pb2.Status(code = 1)
+            
+            
+            # Status.code=2 : Missing groups or choices specification 
+            if len(request.choices) == 0 or len(request.groups) == 0:
+                return eVoting_pb2.Status(code = 2)
+            
+            # Status.code=0 : Election created successfully
+            votes = {}
+            for c in request.choices:
+                votes[c] = 0;
+            self.server.election_table[request.name] = {
+                "end_date" : request.end_date.ToDatetime(),\
+                "groups" : request.groups,\
+                "votes" : votes,\
+                "voters" : []}
+            return eVoting_pb2.Status(code = 0) 
+
+        # Status.code=3 : Unknown error
+        except:
+            return eVoting_pb2.Status(code = 3)
+        
 
     def CastVote(self, request, context):
         print("Received CastVote.")
         print(f'Election Name: {request.election_name}')
         print(f'Choice Name: {request.choice_name}')
-        print(f'Token: {request.token.value.decode()}\n')
-        return eVoting_pb2.Status(code = 4)
+        print(f'Token: {request.token.value}\n')
+
+        try: 
+            # Status.code=1 : Invalid authentication token
+            if request.token.value not in self.server.token_table:
+                return eVoting_pb2.Status(code = 1)
+            if datetime.now() >= self.server.token_table[request.token.value]["expired"]:
+                return eVoting_pb2.Status(code = 1)
+            
+            # Status.code=2 : Invalid election name
+            if request.election_name not in self.server.election_table:
+                return eVoting_pb2.Status(code = 2)
+            if datetime.now() > self.server.election_table[request.election_name]["end_date"]:
+                return eVoting_pb2.Status(code = 2)
+            
+            # Status.code=3 : The voter’s group is not allowed in the election
+            name = self.server.token_table[request.token.value]["name"]
+            group = self.server.registration_table[name]["group"]
+            if group not in self.server.election_table[request.election_name]["groups"]:
+                return eVoting_pb2.Status(code = 3)
+            
+            # Status.code=4 : A previous vote has been cast.
+            if name in self.server.election_table[request.election_name]["voters"]:
+                return eVoting_pb2.Status(code = 4)
+               
+            # Status.code=0 : Successful vote
+            self.server.election_table[request.election_name]["votes"][request.choice_name] += 1
+            self.server.election_table[request.election_name]["voters"].append(name)
+            return eVoting_pb2.Status(code = 0)
+        
+        # Status.code=5 : Unknown error
+        except:
+            return eVoting_pb2.Status(code = 5)
+
 
     def GetResult(self, request, context):
         print("Received GetResult.")
         print(f'Election Name: {request.name}\n')
 
         result = eVoting_pb2.ElectionResult()
-        result.status = 5
+        
+        # ElectionResult.status = 1: Non-existent election
+        if request.name not in self.server.election_table:
+            result.status = 1
+            return result
+        
+        # ElectionResult.status = 2: The election is still ongoing. Election result is not available yet
+        if datetime.now() > self.server.election_table[request.name]["end_date"]:
+            result.status = 2
+            return result
+        
+        # query is successful: ElectionReuslt.status = 0
+        result.status = 0
 
-        curVoteCount = result.counts.add()
-        curVoteCount.choice_name = 'Bob'
-        curVoteCount.count = 50
+        for choice, ballot in self.server.election_table[request.name]["votes"].items():
+            curVoteCount = result.counts.add()
+            curVoteCount.choice_name = choice
+            curVoteCount.count = ballot
 
-        return result
+        return result 
+    
+    
+    
 
 def serve():
     port = '34652'
